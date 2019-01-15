49cd8f79e1f20ea8232169b6670803caa65b0cf6
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-01-15 18:53:46 +0900

    UPDATE: vue-electron boilerplate

--- README.md
+++ README.md
@@ -1,19 +1,6 @@
 # Dimiwords Client [![Standard - JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
-<img src="./assets/icons/logo.png" width=256 height=256>
-
-[Dimiwords](https://github.com/JunhoYeo/dimiwords)의 PC 앱 클라이언트입니다.
-
-- Dimiwords 계정 연결, 단어장 정보 가져와 온/오프라인으로 사용
-    - 온라인 연결의 경우 기존 Dimiwords 포인트 지급
-
-## Build
-
-### OS X
-```
-electron-packager . --overwrite --platform=darwin --arch=x64 --icon=assets/icons/logo --prune=true --out=build
-```
-
-### Windows
-
-### Linux
+- [x] Discord Rich Presence 적용
+- [ ] 단어 및 단어장 추가, 조회
+- [ ] Dimiwords 계정 연결, 단어장 정보 가져와 온/오프라인으로 게임 플레이
+    - 온라인 연결의 경우 Dimiwords 포인트 지급
---
+++ src/main/index.js
@@ -0,0 +1,75 @@
+'use strict'
+
+import { app, BrowserWindow } from 'electron'
+const client = require('discord-rich-presence')('528625130012934174')
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
+
+function createWindow () {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    width: 750,
+    height: 600,
+    titleBarStyle: 'hiddenInset'
+  })
+
+  mainWindow.loadURL(winURL)
+
+  client.updatePresence({
+    state: '단어 외우는 중',
+    details: 'https://dimiwords.tk',
+    largeImageKey: 'logo',
+    instance: true
+  })
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
